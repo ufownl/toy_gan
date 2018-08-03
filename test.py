a1fb5cd8a55c917550f63b4a55f8bcd73c851af6
ufownl <ufownl@gmail.com>
2018-08-03 18:56:09 +0800

    Add a test script

---
+++ test.py
@@ -0,0 +1,33 @@
+import time
+import argparse
+import mxnet as mx
+import matplotlib.pyplot as plt
+from dataset import visualize
+from toy_gan import Generator
+
+mx.random.seed(int(time.time()))
+
+parser = argparse.ArgumentParser(description="Start a toy_gan tester.")
+parser.add_argument("--device_id", help="select device that the model using (default: 0)", type=int, default=0)
+parser.add_argument("--gpu", help="using gpu acceleration", action="store_true")
+args = parser.parse_args()
+
+if args.gpu:
+    context = mx.gpu(args.device_id)
+else:
+    context = mx.cpu(args.device_id)
+
+batch_size = 32
+seed_size = 128
+filters = 64
+
+net_g = Generator(filters)
+net_g.load_parameters("model/toy_gan.generator.params", ctx=context)
+
+seeds = mx.nd.random_normal(shape=(batch_size, seed_size, 1, 1))
+imgs = net_g(seeds)
+print(imgs)
+for i in range(imgs.shape[0]):
+    plt.subplot(imgs.shape[0] // 8 + 1, 8, i + 1)
+    visualize(imgs[i])
+plt.show()
